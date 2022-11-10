2c9ece0b4bbe1a321332ac8a7e37ff880dc8986d
IagoLSJ <JoseIagoLima@alu.ufc.br>
2022-11-10 16:14:21 -0300

    feat: link activity

--- App/app/src/main/java/com/example/app/ui/Login.java
+++ App/app/src/main/java/com/example/app/ui/Login.java
@@ -2,15 +2,36 @@ package com.example.app.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.app.R;
 
 public class Login extends AppCompatActivity {
-
+    private Button btn;
+    private TextView create;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn = findViewById(R.id.button3);
+        create = findViewById(R.id.textView5);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Loginacess.class));
+            }
+        });
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Logincadastro.class));
+            }
+        });
     }
 }
--- App/app/src/main/java/com/example/app/ui/MainActivity.java
+++ App/app/src/main/java/com/example/app/ui/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.app.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.app.R;
-
+import android.os.Handler;
 public class MainActivity extends AppCompatActivity {
-
+    final Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        },4000);
     }
 }
